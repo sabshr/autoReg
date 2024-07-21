e6caeb96e7ce72e1f7c0ed589374bd3d75f15d76
Shreeram Sabareesan <shreeramsabareesan@gmail.com>
2024-07-20 17:45:19 -0700

    parallel page loading + optimizations (~0.1/0.2s faster)

--- script.js
+++ script.js
@@ -3,38 +3,43 @@ const puppeteer = require('puppeteer');
 (async () => {
     const targetTime = new Date('2024-07-18T11:00:00Z'); // UTC time -- Registration opens at 7:00 AM EST or (2024-07-22T11:00:00Z) in UTC
 
+    
     const browser = await puppeteer.connect({
         browserURL: 'http://localhost:9222',
-
     });
-    console.log('Connected to Browser');
-    const pages = await browser.pages();
-    const page = pages[0]; // Assuming login was done in the first tab
+    
 
-    for (let i = 1; i <= 5; i++) {
-        // Open tab
-        const newPage = await browser.newPage();
-        await newPage.goto('https://sis.jhu.edu/sswf/SSS/EnrollmentCart/SSS_EnrollmentCart.aspx?MyIndex=171958');
-        
+    // Open tabs in parallel
+    const numberOfTabs = 10; // TO EDIT LATER - not sure if I want to run a script that opens the tabs in parallel or in series...
+    const newPages = await Promise.all(Array.from({ length: numberOfTabs }, () => browser.newPage()));
 
-        // Time Delay
+
+    // Haven't tested delay mechanism yet...
+    const waitUntilTargetTime = async () => {
         const currentTime = new Date();
         const timeDelta = targetTime - currentTime;
         if (timeDelta > 0) {
             console.log(`Waiting for ${timeDelta / 1000} seconds until the target time.`);
             await new Promise(resolve => setTimeout(resolve, timeDelta));
         }
+    };
+    await waitUntilTargetTime();
 
-        // Select checkbox
+
+    // Parallel page loading
+    await Promise.all(newPages.map(async (newPage, i) => {
+        await newPage.goto('https://sis.jhu.edu/sswf/SSS/EnrollmentCart/SSS_EnrollmentCart.aspx?MyIndex=171958');
+        
+        // Puppeteer web actions
         await newPage.click('#SelectAllCheckBox');
-
-        // Click the register button
         await newPage.click('#ctl00_contentPlaceHolder_ibEnroll');
 
-        // Wait to ensure the registration completes
+        // Wait to ensure registration completes
         await newPage.waitForSelector('#ctl00_contentPlaceHolder_ibEnroll', { hidden: true });
-        console.log("Registered(" + i + ")");
-        await newPage.close();
-    }
+        console.log(`Registered(${i + 1})`);
+        await newPage.close(); // might be able to remove? -- or would an excess of chrome tabs take up too much memory??
+    }));
+
+
     process.exit();
 })();
