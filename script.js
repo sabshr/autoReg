06604cf16217ef4bd3f49f8a4c4df2e319155573
Shreeram Sabareesan <shreeramsabareesan@gmail.com>
2024-07-21 23:08:06 -0700

    last edits before registration

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const targetTime = new Date('2024-07-18T11:00:00Z'); // UTC time -- Registration opens at 7:00 AM EST or (2024-07-22T11:00:00Z) in UTC
+    const targetTime = new Date('2024-07-22T11:00:00Z'); // UTC time -- Use Google to convert registration time to UTC.
 
     
     const browser = await puppeteer.connect({
@@ -9,20 +9,23 @@ const puppeteer = require('puppeteer');
     });
     
 
-    // Open tabs in parallel
-    const numberOfTabs = 10; // TO EDIT LATER - not sure if I want to run a script that opens the tabs in parallel or in series...
+    // Open tabs in parallel (numberOfTabs can be specified)
+    const numberOfTabs = 10; 
     const newPages = await Promise.all(Array.from({ length: numberOfTabs }, () => browser.newPage()));
 
 
-    // Haven't tested delay mechanism yet...
+    // Delay until targetTime has been reached
     const waitUntilTargetTime = async () => {
         const currentTime = new Date();
         const timeDelta = targetTime - currentTime;
         if (timeDelta > 0) {
             console.log(`Waiting for ${timeDelta / 1000} seconds until the target time.`);
             await new Promise(resolve => setTimeout(resolve, timeDelta));
+        } else {
+            console.log('Target time has already passed. Running script.');
         }
     };
+
     await waitUntilTargetTime();
 
 
@@ -37,7 +40,7 @@ const puppeteer = require('puppeteer');
         // Wait to ensure registration completes
         await newPage.waitForSelector('#ctl00_contentPlaceHolder_ibEnroll', { hidden: true });
         console.log(`Registered(${i + 1})`);
-        await newPage.close(); // might be able to remove? -- or would an excess of chrome tabs take up too much memory??
+        await newPage.close(); // might be able to remove? -- or would an excess of chrome tabs take up too much memory?? 
     }));
 
 
