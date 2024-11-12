9d6dede70eb9920832122c6805a9c781d48262c9
Shreeram Sabareesan <shreeramsabareesan@gmail.com>
2024-11-12 18:28:33 -0500

    time set for spring 2025 registration (7pm EST 11/15/2024)

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const targetTime = new Date('2024-07-22T11:00:00Z'); // UTC time -- Use Google to convert registration time to UTC.
+    const targetTime = new Date('2024-11-15T11:59:58Z'); // UTC time -- Use Google to convert registration time to UTC.
 
     
     const browser = await puppeteer.connect({
         browserURL: 'http://localhost:9222',
     });
     
-
     // Open tabs in parallel (numberOfTabs can be specified)
-    const numberOfTabs = 10; 
+    const numberOfTabs = 20; 
     const newPages = await Promise.all(Array.from({ length: numberOfTabs }, () => browser.newPage()));
 
 
@@ -34,15 +33,20 @@ const puppeteer = require('puppeteer');
         await newPage.goto('https://sis.jhu.edu/sswf/SSS/EnrollmentCart/SSS_EnrollmentCart.aspx?MyIndex=171958');
         
         // Puppeteer web actions
-        await newPage.click('#SelectAllCheckBox');
-        await newPage.click('#ctl00_contentPlaceHolder_ibEnroll');
-
+        try {
+            await newPage.click('#SelectAllCheckBox');
+            await newPage.click('#ctl00_contentPlaceHolder_ibEnroll');
+            try {
+                await newPage.waitForSelector('#ctl00_contentPlaceHolder_ibEnroll', { hidden: true });
+                console.log(`Registered(${i + 1})`);
+            } catch (error) {
+                console.log('Registration Failed');
+            }
+        } catch (error) {
+            console.log('Registration Failed');
+        }
         // Wait to ensure registration completes
-        await newPage.waitForSelector('#ctl00_contentPlaceHolder_ibEnroll', { hidden: true });
-        console.log(`Registered(${i + 1})`);
         await newPage.close(); // might be able to remove? -- or would an excess of chrome tabs take up too much memory?? 
     }));
-
-
     process.exit();
 })();
